b8c3b7b245f24f5a9c02a5b5a06be15c1f608fc2
Moses Ayabiogbe <ayabiogbemoses@gmail.com>
2024-10-09 13:02:26 +0100

    dashboard included

---
+++ app/about-us/page.js
@@ -0,0 +1,14 @@
+export default function AboutUs() {
+    return (
+        <div className="h-screen bg-gray-200 text-center px-3 md:px-8 lg:px-16">
+            <hi> About Us</hi>
+            <p>We provide personal loans with easy payment plans</p>
+
+            <div className="p-10 bg-blue-400 rounded-2xl"  >
+              <h2 className="text-5xl text-center font-[600] leading-[60px]">Find smart money products so you <br/> can move
+                  forward</h2>
+                                  
+            </div>
+        </div>
+    );
+}
