1b73d00d2727484160a722d841d004ced27d2a43
Moses Ayabiogbe <ayabiogbemoses@gmail.com>
2024-10-09 13:46:57 +0100

    dashboard proposal

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -2,6 +2,9 @@ export default function Dashboard () {
     return(
         <main>
             <p>User dashboard</p>
+            <p>Propose a UI design</p>
+            <p>Build the UI</p>
+            <p>Propose possiblle changes</p>
         </main>
     )
 }
