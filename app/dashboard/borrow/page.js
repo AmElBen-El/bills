211f1c28155d7e2d9d32f6ea89ce87435f325ddb
Moses Ayabiogbe <ayabiogbemoses@gmail.com>
2024-10-30 17:09:26 +0100

    updated history dashboard

--- app/dashboard/borrow/page.js
+++ app/dashboard/borrow/page.js
@@ -67,7 +67,7 @@ export default function Borrow() {
         <main className="min-h-screen flex justify-center py-4 md:py-6 lg:py-8 px-4 md:px-12 lg:px-16 bg-gray-100">
             <div className="w-full md:w-[380px] flex flex-col gap-4 border border-gray-200 rounded-md bg-gray-50 p-2 md:p-6">
                 <blockquote className="border-b border-gray-200 pb-3">
-                    <span className="font-thin text-lg text-blue-800"> Get an Instant Loan</span>
+                    <span className="font-thin text-lg text-blue-800">Get an Instant Loan</span>
                 </blockquote>
 
                 <form onSubmit={handleSubmit}>
@@ -96,7 +96,15 @@ export default function Borrow() {
                                      
                                         setRate(12)
                                     } else if (item.days === 90) {
-                                        setRate(9.5)
+                                        setRate(9.5)                                        
+                                    }                                   
+                                    if (item.days === 7) {
+                                        setDays(7)                                        
+                                    } else if (item.days === 30) {
+                                     
+                                        setDays(30)
+                                    } else if (item.days === 90) {
+                                        setDays(90)
                                         
                                     }
                                  }}
@@ -115,7 +123,7 @@ export default function Borrow() {
 
             
                 <div className="flex flex-col gap-3 border-dashed border border-blue-500 p-4 rounded-md">
-                <p className="text-gray-800">Interest rate for ... days</p>
+                <p className="text-gray-800">Interest rate for {days} days</p>
                 <p className="text-6xl text-blue-600">{rate}%</p>
                 </div>
                 
