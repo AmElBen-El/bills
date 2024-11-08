0e64a7c2c2732c77565394b7bf0f7701c8a7e138
Moses Ayabiogbe <ayabiogbemoses@gmail.com>
2024-11-08 14:05:47 +0100

    before 8th oct

--- app/auth/signin/page.js
+++ app/auth/signin/page.js
@@ -1,13 +1,22 @@
 "use client"
+import React from "react";
 import Link from "next/link";
 import {FaGoogle} from "react-icons/fa";
-import {FaXTwitter} from "react-icons/fa6";
-import { signIn,useSession } from "next-auth/react";
+import {FaApple, FaXTwitter} from "react-icons/fa6";
+import { signIn,useSession,signOut } from "next-auth/react";
+import { Button } from "@mui/material";
+import { useRouter } from "next/navigation";
 
 
 export default function Auth (){
 const {data:session} =useSession();
-console.log(session);
+const router =useRouter();
+
+React.useEffect(()=> {
+    if (session?.user){
+        router.push("/dashboard/borrow")
+    }
+},[session])
 
     return (
         <main className="min-h-[520px] flex justify-center bg-gradient-to-b from-gray-50 to-gray-300 py-8 px-2">
@@ -26,12 +35,17 @@ console.log(session);
                             <span className="text-white text-lg">Google account</span>
                         </button>
                     </form>
-                    <form className="mb-4">
-                        <button className="w-full h-[3.2em] flex justify-center items-center gap-2 border-b-2 border-gray-500 bg-black rounded-md">
+                    <form 
+                    action={ () =>{
+                        signIn("twitter")
+                    }}
+                    className="mb-4">
+                        <button type="submit" className="w-full h-[3.2em] flex justify-center items-center gap-2 border-b-2 border-gray-500 bg-black rounded-md">
                             <FaXTwitter className="text-white text-2xl"/>
                             <span className="text-white text-lg">Twitter account</span>
                         </button>
                     </form>
+                   
                     <p className="text-gray-600 text-sm">By clicking the signin in button you confirm that you have read and agreed with our <Link href="#" className="text-gray-800 underline">Terms of Use</Link> and <Link href="#" className="text-gray-800 underline">Privacy Policy</Link></p>
 
                 </div>
