264273807e49068151e983d32cd42264df27d580
Moses Ayabiogbe <ayabiogbemoses@gmail.com>
2024-12-10 21:07:25 +0100

    punycode rm

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 "use server"
 import { redirect } from "next/navigation";
 import Image from "next/image";
-const punycode = require("punycode/");
 
 export default async function Home() {
   redirect("/auth/signin");
